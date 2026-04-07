4d5b3381be05e9565d2209cc53e4eac65188ec76
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-04-07 21:49:39 +0300

    Update6

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from './NoteDetails/NoteDetails.client'; 
+import NoteDetailsClient from './NoteDetails.client'; 
 
 export default async function NoteDetailsPage({ params }: { params: Promise<{ id: string }> }) {
   const queryClient = new QueryClient();
