7aa870cb3638adc094cf5982e8b473ecebe43b05
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-04-07 21:47:13 +0300

    Update5

--- app/notes/[id]/NoteDetails/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails/NoteDetails.client.tsx
@@ -3,7 +3,7 @@
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import css from './NoteDetails.client.module.css';
+// import css from './NoteDetails.client.module.css';
 
 export default function NoteDetailsClient() {
   const params = useParams();
